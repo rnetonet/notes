3005f3fbbd937751c791a76b3c782e4149ec5e7e
rnetodev <rneto@rneto.dev>
2019-08-20 17:15:56 -0300

    up

--- books/django_2_by_example_packt/mysite/blog/views.py
+++ books/django_2_by_example_packt/mysite/blog/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    context = {
-        "posts": posts
-    }
-    return render(request, "blog/post/list.html", context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 1
+    template_name = "blog/post/list.html"
+
+# def post_list(request):
+#     posts = Post.published.all()
+#
+#     paginator = Paginator(posts, 1)
+#     page = request.GET.get("page")
+#
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         "posts": posts
+#     }
+#     return render(request, "blog/post/list.html", context)
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
+        Post,
         status="published",
         publish__year=year,
         publish__month=month,
@@ -20,4 +40,4 @@ def post_detail(request, year, month, day, post):
     )
     context = {"post": post}
 
-    return render(request, "blog/post/detail.html", post)
+    return render(request, "blog/post/detail.html", context)
