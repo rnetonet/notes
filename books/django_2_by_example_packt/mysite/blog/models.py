36703f8ce15bd35a07be25763cf0d1c712378a37
rnetodev <rneto@rneto.dev>
2019-08-26 00:25:17 -0300

    gitignore

--- books/django_2_by_example_packt/mysite/blog/models.py
+++ books/django_2_by_example_packt/mysite/blog/models.py
@@ -1,45 +1,68 @@
-from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db import models
 from django.urls import reverse
+from django.utils import timezone
+
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
 class PostPublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status='published')
+        return super().get_queryset().filter(status="published")
+
 
 class Post(models.Model):
-    STATUS_CHOICES = (
-        ("draft", "Draft"),
-        ("published", "Published"),
-    )
+    STATUS_CHOICES = (("draft", "Draft"), ("published", "Published"))
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date="publish")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    tags = TaggableManager()
+
 
     objects = models.Manager()
     published = PostPublishedManager()
 
     class Meta:
-        ordering = ("-publish", )
+        ordering = ("-publish",)
 
     def get_absolute_url(self):
         return reverse(
             "blog:post_detail",
-            args=[
-                self.publish.year,
-                self.publish.month,
-                self.publish.day,
-                self.slug
-            ]
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
         )
 
     def __str__(self):
         return self.title
 
+
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(active=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    actives = ActiveCommentsManager()
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
--- books/django_2_by_example_packt/mysite/blog/views.py
+++ books/django_2_by_example_packt/mysite/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-
-from .models import Post
+from django.core.mail import send_mail
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 class PostListView(ListView):
@@ -11,23 +12,43 @@ class PostListView(ListView):
     paginate_by = 1
     template_name = "blog/post/list.html"
 
-# def post_list(request):
-#     posts = Post.published.all()
-#
-#     paginator = Paginator(posts, 1)
-#     page = request.GET.get("page")
-#
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#
-#     context = {
-#         "posts": posts
-#     }
-#     return render(request, "blog/post/list.html", context)
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = f"{data['name']} ({data['email']}) recommends to you the article {post.title}"
+            message = f"""
+                Read: {post.title} at {post_url}.
+
+                {data['name']} comments:
+                {data['comments']}
+            """
+            send_mail(
+                subject,
+                message,
+                "admin@blog.me",
+                [data["to"]],
+                fail_silently=False
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    context = {
+        "post": post,
+        "form": form,
+        "sent": sent
+    }
+
+    return render(request, "blog/post/share.html", context)
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -36,8 +57,24 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day,
-        slug=post
+        slug=post,
     )
-    context = {"post": post}
 
+    new_comment = None
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+
+    context = {
+        "post": post,
+        "comments": post.comments.filter(active=True),
+        "new_comment": new_comment,
+        "comment_form": comment_form
+    }
     return render(request, "blog/post/detail.html", context)
