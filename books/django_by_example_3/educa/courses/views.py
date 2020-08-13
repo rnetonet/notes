8d1ccb328e9b0bca010cb612229721e4e8a4d255
rnetonet <rneto@rneto.net>
2020-08-12 23:47:18 -0300

    educa

---
+++ books/django_by_example_3/educa/courses/views.py
@@ -0,0 +1,13 @@
+from typing import List
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Course
+
+# Create your views here.
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = "courses/manage/course/list.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
