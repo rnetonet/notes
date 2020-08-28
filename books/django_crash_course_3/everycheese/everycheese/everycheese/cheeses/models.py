8038160df7fe5aa64174659f8af0d1eee18b24d0
rnetonet <rneto@rneto.net>
2020-08-28 17:07:56 -0300

    up

--- books/django_crash_course_3/everycheese/everycheese/everycheese/cheeses/models.py
+++ books/django_crash_course_3/everycheese/everycheese/everycheese/cheeses/models.py
@@ -1,5 +1,6 @@
 from autoslug import AutoSlugField
 from django.db import models
+from django_countries.fields import CountryField
 from model_utils.models import TimeStampedModel
 
 
@@ -11,11 +12,18 @@ class Cheese(TimeStampedModel):
         SEMI_HARD = "semi-hard", "Semi-Hard"
         HARD = "hard", "Hard"
 
-
     name = models.CharField("Name of cheese", max_length=255)
-    slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from="name")
+    slug = AutoSlugField(
+        "Cheese Address", unique=True, always_update=False, populate_from="name"
+    )
     description = models.TextField("Description", blank=True)
-    firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+    firmness = models.CharField(
+        "Firmness",
+        max_length=20,
+        choices=Firmness.choices,
+        default=Firmness.UNSPECIFIED,
+    )
+    country_of_origin = CountryField("Country of origin", blank=True)
 
     def __str__(self) -> str:
         return self.name
--- books/django_crash_course_3/everycheese/everycheese/everycheese/cheeses/tests/test_models.py
+++ books/django_crash_course_3/everycheese/everycheese/everycheese/cheeses/tests/test_models.py
@@ -1,15 +1,12 @@
 import pytest
 
 from everycheese.cheeses.models import Cheese
+from everycheese.cheeses.tests.factories import CheeseFactory
 
 pytestmark = pytest.mark.django_db
 
 
 def test_cheese_string_representation():
-    cheese = Cheese.objects.create(
-        name='Stracchino',
-        description='A cheese',
-        firmness=Cheese.Firmness.SOFT,
-    )
+    cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
