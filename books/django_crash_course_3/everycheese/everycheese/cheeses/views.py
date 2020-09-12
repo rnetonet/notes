3febb2790f2c1631710b5b45aa2e0317210cb4e5
rnetonet <rneto@rneto.net>
2020-09-11 22:59:54 -0300

    up

--- books/django_crash_course_3/everycheese/everycheese/cheeses/views.py
+++ books/django_crash_course_3/everycheese/everycheese/cheeses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 from .models import Cheese
@@ -9,5 +10,10 @@ from .models import Cheese
 class CheeseListView(ListView):
     model = Cheese
 
+
 class CheeseDetailView(DetailView):
+    model = Cheese
+
+
+class CheeseCreateView(CreateView):
     model = Cheese
